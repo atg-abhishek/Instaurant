dfc559c96d2d51577923d8e6dd3819ec1f1bc255
jessyhann <jessyhann@gmail.com>
2015-10-10 17:06:19 -0400

    added function to read/write user authentication

--- firebaseConn.py
+++ firebaseConn.py
@@ -8,9 +8,11 @@ def write_basic_profile_to_db(username, user_data):
 	result = firebase_obj.put(url='/profile_data', name=username,data=user_data)
 	#result=firebase.post('test/1',{'name':'jack','age':'100'})
 	#result = firebase.get('/username', None)
+
 #get all profile info base on given username
 def fetch_basic_profile_by_username(username):
 	return firebase_obj.get('/profile_data', username)
+
 #check if a user exists by username
 def does_user_exist(username):
 	profile = firebase_obj.get('/profile_data', username)
@@ -18,3 +20,11 @@ def does_user_exist(username):
 		return "False"
 	else:
 		return "True"
+
+#write user auth to database
+def write_user_auth_to_db(username, token):
+	result = firebase_obj.put(url='/user_auth', name=username,data=token)
+	
+#get token by given username
+def fetch_token_by_username(username):
+	return firebase_obj.get('/user_auth', username)
