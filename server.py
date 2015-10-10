e3be46131d27975c8f5a518982dbda61c1d458f6
atg.abhishek <atg.abhishek@gmail.com>
2015-10-10 14:29:38 -0400

    login

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask 
-
+import simplejson as json
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Welcome to Instaurant'
 
-
+@app.route('/login', methods=['POST'])
+def login():
+	username = request.form['username']
+	token = request.form['token']
+	login_details = {'username' : username, 'token' : token}
+	with open('./datafiles/'+username+'_login.json', 'w') as outfile:
+		json.dump(login_details, outfile)
 
 if __name__ == '__main__':
 	app.run(debug=True)
